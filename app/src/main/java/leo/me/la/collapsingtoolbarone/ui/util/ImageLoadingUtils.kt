ebe8e8bc9c2e57c5f6a58a3a69c1c19a34f81637
Giang Pham <conmeobeo96@gmail.com>
2018-10-19 23:23:16 +0300

    Add image loading utils

---
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/ui/util/ImageLoadingUtils.kt
@@ -0,0 +1,72 @@
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import leo.me.la.collapsingtoolbarone.ui.util.GlideApp
+import com.bumptech.glide.request.target.Target
+
+fun ImageView.loadUri(
+    uri: Uri?,
+    cache: Boolean = true,
+    noFade: Boolean = false,
+    noPlaceholder: Boolean = false,
+    thumbnail: Uri? = null,
+    onSuccess: (() -> Unit)? = null,
+    onError: (() -> Unit)? = null
+) {
+    GlideApp.with(context)
+        .load(uri)
+        .diskCacheStrategy(if (cache) DiskCacheStrategy.AUTOMATIC else DiskCacheStrategy.NONE)
+        .skipMemoryCache(!cache)
+        .thumbnail(GlideApp.with(context).load(thumbnail))
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                onError?.invoke()
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                onSuccess?.invoke()
+                return false
+            }
+        })
+        .centerCrop()
+        .apply {
+            if (!noPlaceholder) placeholder(android.R.color.darker_gray)
+            if (!noFade) transition(DrawableTransitionOptions().crossFade())
+        }
+        .into(this)
+}
+
+fun ImageView.loadUri(
+    uri: String?,
+    cache: Boolean = true,
+    noFade: Boolean = false,
+    noPlaceholder: Boolean = false,
+    thumbnail: String? = null,
+    onSuccess: (() -> Unit)? = null,
+    onError: (() -> Unit)? = null
+) = loadUri(
+    uri?.let(Uri::parse),
+    cache,
+    noFade,
+    noPlaceholder,
+    thumbnail?.let(Uri::parse),
+    onSuccess,
+    onError
+)
