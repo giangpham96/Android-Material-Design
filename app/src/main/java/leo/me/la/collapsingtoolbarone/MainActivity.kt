9be052e7dae53387c53d88e45615dbc3ace64a18
Giang Pham <conmeobeo96@gmail.com>
2018-10-19 23:30:04 +0300

    Add trend recycler view

--- app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
@@ -2,11 +2,19 @@ package leo.me.la.collapsingtoolbarone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout.HORIZONTAL
+import kotlinx.android.synthetic.main.activity_main.trends
+import leo.me.la.collapsingtoolbarone.ui.adapter.TrendAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trends.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity, HORIZONTAL, false)
+            adapter = TrendAdapter()
+        }
     }
 }
