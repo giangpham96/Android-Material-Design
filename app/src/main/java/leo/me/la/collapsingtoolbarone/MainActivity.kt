90a16a46585663e7b9d6496ea6c70c42cff9b022
Giang Pham <conmeobeo96@gmail.com>
2018-10-20 08:51:04 +0300

    Preload image on recyclerview for better UX

--- app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
@@ -7,15 +7,23 @@ import android.widget.LinearLayout.HORIZONTAL
 import kotlinx.android.synthetic.main.activity_main.trends
 import leo.me.la.collapsingtoolbarone.ui.adapter.TrendAdapter
 import android.support.v7.widget.LinearSnapHelper
+import com.bumptech.glide.integration.recyclerview.RecyclerViewPreloader
+import com.bumptech.glide.util.ViewPreloadSizeProvider
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val preloadSizeProvider = ViewPreloadSizeProvider<Trend>()
+        val trendAdapter = TrendAdapter(this@MainActivity, preloadSizeProvider)
+        val preloader = RecyclerViewPreloader<Trend>(this, trendAdapter, preloadSizeProvider, 5)
         trends.apply {
             layoutManager = LinearLayoutManager(this@MainActivity, HORIZONTAL, false)
-            adapter = TrendAdapter()
+            adapter = trendAdapter
+            addOnScrollListener(preloader)
         }
         LinearSnapHelper().attachToRecyclerView(trends)
     }
--- app/src/main/java/leo/me/la/collapsingtoolbarone/ui/adapter/TrendAdapter.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/ui/adapter/TrendAdapter.kt
@@ -1,17 +1,28 @@
 package leo.me.la.collapsingtoolbarone.ui.adapter
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.ListPreloader
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.util.ViewPreloadSizeProvider
+import configure
 import leo.me.la.collapsingtoolbarone.R
 import leo.me.la.collapsingtoolbarone.Trend
-import loadUri
+import leo.me.la.collapsingtoolbarone.ui.util.GlideApp
+import leo.me.la.collapsingtoolbarone.ui.util.GlideRequest
 
 class TrendAdapter(
-    private val trends : List<Trend> = listOf(
+    context: Context,
+    private val preloadModelProvider: ViewPreloadSizeProvider<Trend>,
+    private val trends: List<Trend> = listOf(
         Trend("Fitness", "https://hdqwalls.com/walls/thumb/gym-girl-8k-image.jpg"),
         Trend("Animals", "https://render.fineartamerica.com/images/rendered/search/print/images-medium-5/1-panda-king-wu.jpg"),
         Trend("Outdoor", "http://www.ablecanopies.co.uk/blog/wp-content/uploads/2015/11/outdoor-learning-for-secondary-schools-small-200x300.jpg"),
@@ -23,11 +34,23 @@ class TrendAdapter(
         Trend("Food", "http://www.restauranttristan.co.uk/wp-content/uploads/2016/10/tristan-dish-1111.jpg"),
         Trend("Style", "http://img.saobiz.net/d/2016/06/nang-tho-xu-hue.jpg")
     )
-) : RecyclerView.Adapter<TrendAdapter.TrendVH>() {
+) : RecyclerView.Adapter<TrendAdapter.TrendVH>(), ListPreloader.PreloadModelProvider<Trend> {
+
+    private val thumbnailRequest = GlideApp.with(context)
+        .asDrawable()
+        .diskCacheStrategy(DiskCacheStrategy.DATA)
+        .transition(withCrossFade())
+
+    private val fullRequest = GlideApp.with(context).asDrawable()
+
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): TrendVH {
         return TrendVH(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_trend, parent, false)
-        )
+            LayoutInflater.from(parent.context).inflate(R.layout.item_trend, parent, false),
+            fullRequest,
+            thumbnailRequest
+        ).also {
+            preloadModelProvider.setView(it.itemView)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -38,13 +61,30 @@ class TrendAdapter(
         holder.bind(trends[position])
     }
 
-    class TrendVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    override fun getPreloadItems(position: Int): List<Trend> {
+        return listOf(trends[position])
+    }
+
+    override fun getPreloadRequestBuilder(item: Trend): RequestBuilder<Drawable>? {
+        return fullRequest.load(item.imageUrl)
+            .thumbnail(thumbnailRequest.load(item.imageUrl))
+            .configure()
+    }
+
+    class TrendVH(
+        itemView: View,
+        private val fullRequest: GlideRequest<Drawable>,
+        private val thumbnailRequest: GlideRequest<Drawable>
+    ) : RecyclerView.ViewHolder(itemView) {
         private val image = itemView.findViewById<ImageView>(R.id.image)
         private val name = itemView.findViewById<TextView>(R.id.name)
 
         fun bind(trend: Trend) {
             name.text = trend.name
-            image.loadUri(trend.imageUrl)
+            fullRequest.load(trend.imageUrl)
+                .thumbnail(thumbnailRequest.load(trend.imageUrl))
+                .configure()
+                .into(image)
         }
     }
 }
