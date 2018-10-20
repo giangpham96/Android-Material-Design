7185801f17d9477d1c35340a1b38fec0ad72d056
Giang Pham <conmeobeo96@gmail.com>
2018-10-20 15:35:22 +0300

    Add dummy data

--- app/src/main/java/leo/me/la/collapsingtoolbarone/ui/adapter/PostAdapter.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/ui/adapter/PostAdapter.kt
@@ -36,6 +36,36 @@ class PostAdapter(
             3231,
             "http://dollars-bbs.org/sports/src/1433367290411.jpg",
             listOf("Fitness", "Animal", "Funny", "Cat", "Dumbbell", "Workout")
+        ),
+        Post(
+            441,
+            "https://www.diabetesprevention.pitt.edu/wps/wp-content/uploads/2015/07/feature-July2015a.jpg",
+            listOf("Lifestyle", "Sport", "Food", "Fitness", "Running", "Healthy")
+        ),
+        Post(
+            12039,
+            "https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15972427_262180554195307_8376375207276903176_o.jpg?_nc_cat=108&_nc_ht=scontent-arn2-1.xx&oh=79e9cd50bbaacdc5761d81c63a312007&oe=5C5C1548",
+            listOf("Love", "Civil War", "Funny", "Photoshop", "Dab", "Ba vi")
+        ),
+        Post(
+            9021,
+            "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-0/p206x206/25660120_1848015098605167_8097854011213076199_n.jpg?_nc_cat=109&_nc_ht=scontent-arn2-1.xx&oh=e2290726497790ed76f5cf390c40fa4d&oe=5C3F31F7",
+            listOf("Self", "Beauty", "Pretty", "Yearbook")
+        ),
+        Post(
+            2311,
+            "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/10689984_373320009488245_7199137391190762517_n.jpg?_nc_cat=107&_nc_ht=scontent-arn2-1.xx&oh=442045d060e5f2c1aef465b709b68d73&oe=5C5C326B",
+            listOf("Freshman")
+        ),
+        Post(
+            102,
+            "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/10406975_482948431842940_4537894200487913963_n.png?_nc_cat=100&_nc_ht=scontent-arn2-1.xx&oh=9061b3256fae862bff0fb6d73f02c387&oe=5C4768BF",
+            listOf("K-pop", "Idol", "Red Velvet", "Quote", "Singer")
+        ),
+        Post(
+            7249,
+            "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/43470085_1908189912822137_5102037478219448320_o.jpg?_nc_cat=101&_nc_ht=scontent-arn2-1.xx&oh=11be085ebcbc3181fa5ee71fe2ba7445&oe=5C40B524",
+            listOf("Vintage", "Ancient", "Countryside", "1980s")
         )
     )
 ) : RecyclerView.Adapter<PostAdapter.PostVH>(), ListPreloader.PreloadModelProvider<Post> {
