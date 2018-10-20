c39ac75643f705faee1356aea165c4dcaf436e7c
Giang Pham <conmeobeo96@gmail.com>
2018-10-20 14:18:56 +0300

    Add posts to Ui

--- app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
@@ -7,9 +7,11 @@ import android.widget.LinearLayout.HORIZONTAL
 import kotlinx.android.synthetic.main.activity_main.trends
 import leo.me.la.collapsingtoolbarone.ui.adapter.TrendAdapter
 import android.support.v7.widget.LinearSnapHelper
+import android.widget.LinearLayout.VERTICAL
 import com.bumptech.glide.integration.recyclerview.RecyclerViewPreloader
 import com.bumptech.glide.util.ViewPreloadSizeProvider
-
+import kotlinx.android.synthetic.main.activity_main.posts
+import leo.me.la.collapsingtoolbarone.ui.adapter.PostAdapter
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +28,9 @@ class MainActivity : AppCompatActivity() {
             addOnScrollListener(preloader)
         }
         LinearSnapHelper().attachToRecyclerView(trends)
+        posts.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity, VERTICAL, false)
+            adapter = PostAdapter()
+        }
     }
 }
