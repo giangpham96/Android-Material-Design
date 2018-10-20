d997c426646a717b95527a55b060b981b48956a8
Giang Pham <conmeobeo96@gmail.com>
2018-10-20 15:12:31 +0300

    Make Post entity implement ImageURLProvider

--- app/src/main/java/leo/me/la/collapsingtoolbarone/Entities.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/Entities.kt
@@ -1,5 +1,7 @@
 package leo.me.la.collapsingtoolbarone
 
+import leo.me.la.collapsingtoolbarone.ui.preloadsizeprovider.ImageURLProvider
+
 data class Trend(
     val name: String,
     val imageUrl: String
@@ -9,4 +11,8 @@ data class Post(
     val views: Int,
     val imageUrl: String,
     val tags: List<String>
-)
+) : ImageURLProvider {
+    override fun provideUrl(): String? {
+        return imageUrl
+    }
+}
