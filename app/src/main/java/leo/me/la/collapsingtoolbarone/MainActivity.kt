46c2573006e8c835a64ad584dcc4e89021e18c3f
Giang Pham <conmeobeo96@gmail.com>
2018-10-21 11:36:23 +0300

    Add fab and animation to reveal actions

--- app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
+++ app/src/main/java/leo/me/la/collapsingtoolbarone/MainActivity.kt
@@ -1,5 +1,9 @@
 package leo.me.la.collapsingtoolbarone
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -15,6 +19,13 @@ import leo.me.la.collapsingtoolbarone.ui.adapter.PostAdapter
 import leo.me.la.collapsingtoolbarone.ui.preloadsizeprovider.RemotePreloadSizeProvider
 import android.app.Activity
 import android.graphics.Point
+import android.support.v4.widget.NestedScrollView
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.view.animation.Interpolator
+import kotlinx.android.synthetic.main.activity_main.actions
+import kotlinx.android.synthetic.main.activity_main.content_scorller
+import kotlinx.android.synthetic.main.activity_main.fab
 import kotlinx.android.synthetic.main.activity_main.name
 
 class MainActivity : AppCompatActivity() {
@@ -40,10 +51,142 @@ class MainActivity : AppCompatActivity() {
             addOnScrollListener(postPreloader)
         }
         name.isSelected = true
+        content_scorller.setOnScrollChangeListener { nestedScrollView: NestedScrollView?, x: Int, y: Int, oldX: Int, oldY: Int ->
+            val hideActionsAnimatorSet = fab.getTag(R.string.animator_hide) as? AnimatorSet
+            val revealActionsAnimator = fab.getTag(R.string.animator_reveal) as? AnimatorSet
+            if (hideActionsAnimatorSet?.isRunning == true || revealActionsAnimator?.isRunning == true) {
+                return@setOnScrollChangeListener
+            }
+            if (actions.visibility == View.VISIBLE) {
+                val xAnimator = ObjectAnimator.ofFloat(
+                    fab,
+                    "x",
+                    fab.getTag(R.string.initial_x) as Float
+                )
+                val yAnimator = ObjectAnimator.ofFloat(
+                    fab,
+                    "y",
+                    fab.getTag(R.string.initial_y) as Float
+                ).apply {
+                    interpolator = Interpolator { input ->
+                        1 - (Math.pow(1.2, -30 * (input - 0.2)) * Math.sin(2 * Math.PI * (input - 0.2) + Math.PI / 2)).toFloat()
+                    }
+                }
+                val hideActionsAnimator = ViewAnimationUtils.createCircularReveal(
+                    actions,
+                    (actions.width / 2),
+                    (actions.height / 2),
+                    actions.width / 2f * 1.2f,
+                    0f
+                ).apply {
+                    duration = 300
+                    addListener(object : Animator.AnimatorListener {
+                        override fun onAnimationRepeat(animation: Animator?) {}
+
+                        override fun onAnimationEnd(animation: Animator?) {
+                            fab.show()
+                            actions.visibility = View.INVISIBLE
+                        }
+
+                        override fun onAnimationCancel(animation: Animator?) {}
+
+                        override fun onAnimationStart(animation: Animator?) {}
+
+                    })
+                }
+                AnimatorSet().apply {
+                    playSequentially(
+                        if (actions.visibility == View.VISIBLE) {
+                            listOf(hideActionsAnimator)
+                        } else {
+                            emptyList()
+                        } + listOf(
+                            AnimatorSet().apply {
+                                playTogether(xAnimator, yAnimator)
+                                duration = 300
+                            },
+                            // Ensure that the fab is shown at least half a second before it is
+                            // hidden by scrolling down behavior
+                            ValueAnimator.ofInt(0, 1).apply {
+                                duration = 500
+                            }
+                        )
+                    )
+                }.also {
+                    fab.setTag(R.string.animator_hide, it)
+                }.start()
+            } else {
+                if (y > oldY) {
+                    fab.hide()
+                } else {
+                    fab.show()
+                }
+            }
+        }
+        fab.apply {
+            post {
+                setTag(R.string.initial_x, fab.x)
+                setTag(R.string.initial_y, fab.y)
+            }
+            setOnClickListener {
+                val xAnimator = ObjectAnimator.ofFloat(
+                    it,
+                    "x",
+                    actions.width / 2F - it.width / 2F
+                )
+                val yAnimator = ObjectAnimator.ofFloat(
+                    it,
+                    "y",
+                    actions.y + actions.height / 2 - it.height / 2
+                ).apply {
+                    interpolator = Interpolator { input ->
+                        1 - (Math.pow(1.2, -30 * (input - 0.2)) * Math.sin(2 * Math.PI * (input - 0.2) + Math.PI / 2)).toFloat()
+                    }
+                }
+                val revealActionsAnimator = ViewAnimationUtils.createCircularReveal(
+                    actions,
+                    (actions.width / 2),
+                    (actions.height / 2),
+                    0f,
+                    actions.width / 2f * 1.2f
+                ).apply {
+                    duration = 300
+                }
+                AnimatorSet().apply {
+                    playSequentially(
+                        AnimatorSet().apply {
+                            duration = 300
+                            playTogether(xAnimator, yAnimator)
+                            addListener(object : Animator.AnimatorListener {
+                                override fun onAnimationRepeat(animation: Animator?) {}
+
+                                override fun onAnimationEnd(animation: Animator?) {
+                                    fab.hide()
+                                    actions.visibility = View.VISIBLE
+                                }
+
+                                override fun onAnimationCancel(animation: Animator?) {}
+
+                                override fun onAnimationStart(animation: Animator?) {}
+
+                            })
+                        },
+                        revealActionsAnimator,
+                        // Ensure that the actions list is shown at least half a second before it
+                        // is hidden by scrolling behavior
+                        ValueAnimator.ofInt(0, 1).apply {
+                            duration = 500
+                        }
+                    )
+                }.also { animator ->
+                    it.setTag(R.string.animator_reveal, animator)
+                }.start()
+            }
+        }
     }
 }
 
-fun Activity.getScreenWidth() : Int {
+fun Activity.getScreenWidth(): Int {
     val display = windowManager.defaultDisplay
     val size = Point()
     display.getSize(size)
